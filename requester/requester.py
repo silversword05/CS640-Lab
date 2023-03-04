85f1d4827c4664b2ee2717252b48b8e17e9465c4
silversword <amondal5@wisc.edu>
2023-03-04 14:12:32 -0600

    Added print statements

--- requester/requester.py
+++ requester/requester.py
@@ -1,6 +1,7 @@
 import argparse
 import socket
 import struct
+from datetime import datetime
 
 from collections import defaultdict
 
@@ -28,28 +29,56 @@ def read_tacker():
         TRACKER_TABLE[tokens[0]][int(tokens[1])] = (tokens[2], int(tokens[3]))
 
 
-def receive_file(receive_port: int):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('', receive_port))
+def print_packet(packet_type: str, sender_addr: str, sender_from_port: int, seq_num: int, payload: bytes):
+    if packet_type == 'D':
+        print("DATA Packet")
+    if packet_type == 'E':
+        print("End Packet")
+    print(f"recv time:    {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}")
+    print(f"sender addr:  {sender_addr}:{sender_from_port}")
+    print(f"sequence:     {seq_num}")
+    print(f"length:       {len(payload)}")
+    print(f"payload:      {payload[:4].decode('utf-8')}")
+    print()
 
+
+def print_summary(sender_addr: str, sender_from_port: int, total_packets: int, total_bytes: int, total_duration: int):
+    print("Summary")
+    print(f"sender addr:             {sender_addr}:{sender_from_port}")
+    print(f"Total Data packets:      {total_packets}")
+    print(f"Total Data bytes:        {total_bytes}")
+    print(f"Average packets/second:  {int(total_packets * 1000 / total_duration) if total_duration != 0 else 0}")
+    print(f"Duration of the test:    {total_duration}  ms")
+
+
+def receive_file(sock: socket):
+    packet_count = byte_count = 0
+    start = end = None
     while True:
         packet, from_address = sock.recvfrom(BUF_SIZE)
+        if start is None:
+            start = datetime.now()
         header, data = Header(*struct.unpack("!c2I", packet[:9])), packet[9:]
-        print("received message: %s from %s" % (header, from_address))
-        print(len(data))
+        print_packet(header.type, from_address[0], from_address[1], header.seq_no, data)
+        packet_count += 1
+        byte_count += len(data)
         if header.type == 'E':
+            end = datetime.now()
+            print_summary(from_address[0], from_address[1], packet_count, byte_count, int((end - start).total_seconds() * 1000))
             break
 
 
 def send_file_request(receive_port: int, filename: str):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', receive_port))
+
     assert filename in TRACKER_TABLE, "Filename not in tracker"
     file_table = TRACKER_TABLE[filename]
     i = 1
     while i in file_table:
         packet = struct.pack("!c2I", b'R', socket.htonl(0), socket.htonl(0)) + bytes(filename, "utf-8")
         sock.sendto(packet, (socket.gethostbyname(file_table[i][0]), file_table[i][1]))
-        receive_file(receive_port)
+        receive_file(sock)
         i += 1
 
 
